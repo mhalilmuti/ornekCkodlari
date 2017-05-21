1bd332e6b87c6e389f8ec742975d0d22fc9b333f
mhalilmuti <mhalilmuti@hotmail.com>
2017-05-21 10:02:07 +0300

    HelloWorld

---
+++ GirisSeviyesiOrnekleri/HelloWord.c
@@ -0,0 +1,20 @@
+//Bu C kod parcasi Muhammet Halil Muti Tarafindan
+//mhmuti.wordpress.com adresindeki
+// C egitimi icin hazirlanmistir. 
+
+//Kutuphane kismi
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//main fonksiyonu
+int main()
+{
+    
+    //Ekrana deger yazdirma islemi
+    printf("Hello world!\n");
+    
+    
+    //fonksiyon sonu
+    return 0;
+}
