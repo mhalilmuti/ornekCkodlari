630751d70ae206271fea71d39b457c96c769774f
mhalilmuti <mhalilmuti@hotmail.com>
2017-05-21 10:20:38 +0300

    GirilenSayiyiEkranaYazdirma

---
+++ GirisSeviyesiOrnekleri/GirilenSayiyiEkranaYazdirmaOrnegi.c
@@ -0,0 +1,27 @@
+//Bu C kod parcasi Muhammet Halil Muti Tarafindan
+//mhmuti.wordpress.com adresindeki
+// C egitimi icin hazirlanmistir. 
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    //Degisken taimlama
+    int sayi,sonuc;
+
+    printf("Sayi Degerini Giriniz:");
+
+    //Kullanicidan deger alma
+    scanf("%d",&sayi);
+
+    printf("Girilen Deger: %d \n",sayi);
+
+    //degiskenlere deger atama
+    sonuc = sayi * 2 ;
+
+    printf("Girilen Degerin iki kati: %d \n",sonuc);
+
+    system("PAUSE");
+    return 0;
+}
